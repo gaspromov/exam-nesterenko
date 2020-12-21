61cd105fbac25dfb5e9ceb49f2229ebfc58a017c
gaspromov <gaspromov@mail.ru>
2020-12-21 12:21:48 -0200

    Готово

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
+import { NewItemComponent } from './components/new-item/new-item.component';
+import { ViewComponent } from './components/view/view.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'main', component: MainComponent },
+  { path: 'view', component: ViewComponent },
+  { path: 'edit/:id', component: NewItemComponent },
+  { path: 'add', component: NewItemComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { ViewComponent } from './components/view/view.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NewItemComponent } from './components/new-item/new-item.component';
+import { SortPipe } from './pipes/sort/sort.pipe';
+import { FilterCategoryPipe } from './pipes/filter-category.pipe';
+import { FilterStockPipe } from './pipes/filterStock/filter-stock.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ViewComponent,
+    NavbarComponent,
+    NewItemComponent,
+    SortPipe,
+    FilterCategoryPipe,
+    FilterStockPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
